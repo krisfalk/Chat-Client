ecad15d468bbd18fb21f61ad45721b3cbd2e6cae
krisfalk <krisfalk2007@hotmail.com>
2016-09-06 07:42:47 -0500

    new files added

---
+++ frm_Main.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Game
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.mainChat = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txt_text = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.disconBtn = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 315);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(593, 25);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(49, 20);
+            this.statusLabel.Text = "Status";
+            // 
+            // mainChat
+            // 
+            this.mainChat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainChat.Location = new System.Drawing.Point(4, 19);
+            this.mainChat.Margin = new System.Windows.Forms.Padding(4);
+            this.mainChat.Name = "mainChat";
+            this.mainChat.Size = new System.Drawing.Size(408, 199);
+            this.mainChat.TabIndex = 1;
+            this.mainChat.Text = "";
+            this.mainChat.TextChanged += new System.EventHandler(this.mainChat_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.mainChat);
+            this.groupBox1.Location = new System.Drawing.Point(16, 55);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(416, 222);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Main Chat";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(444, 282);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(137, 28);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txt_text
+            // 
+            this.txt_text.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_text.Location = new System.Drawing.Point(20, 286);
+            this.txt_text.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_text.Name = "txt_text";
+            this.txt_text.Size = new System.Drawing.Size(411, 22);
+            this.txt_text.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(4, 19);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(137, 237);
+            this.listBox1.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(440, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(145, 260);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "All Client";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(74, 23);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(131, 22);
+            this.txtName.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(213, 20);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 28);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // disconBtn
+            // 
+            this.disconBtn.Location = new System.Drawing.Point(321, 20);
+            this.disconBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.disconBtn.Name = "disconBtn";
+            this.disconBtn.Size = new System.Drawing.Size(100, 28);
+            this.disconBtn.TabIndex = 9;
+            this.disconBtn.Text = "Disconnect";
+            this.disconBtn.UseVisualStyleBackColor = true;
+            this.disconBtn.Click += new System.EventHandler(this.disconBtn_Click);
+            // 
+            // FormMain
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 340);
+            this.Controls.Add(this.disconBtn);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.txt_text);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.RichTextBox mainChat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txt_text;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button disconBtn;
+    }
+}
+
---
+++ frm_Main.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class FormMain : Form
+    {
+        private  Socket _clientSocket;
+        byte[] receivedBuf = new byte[1024];
+        string _Name { get; set; }
+        public FormMain()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private void ReceiveData(IAsyncResult result)
+        {
+            Socket socket = (Socket)result.AsyncState;
+            int received = socket.EndReceive(result);
+            byte[] dataBuf = new byte[received];
+            Array.Copy(receivedBuf, dataBuf, received);
+            string check = Encoding.ASCII.GetString(dataBuf);
+            if(CheckIfDisconnect(check, socket))
+            {
+                return;
+            }
+            if (check.Contains(">>") && check.Substring(3, check.Length - 3).Contains(">>") == false)
+            {
+                AddNames(check);
+            }
+            else
+            {
+                mainChat.Text = mainChat.Text + (Encoding.ASCII.GetString(dataBuf));
+            }
+            _clientSocket.BeginReceive(receivedBuf, 0, receivedBuf.Length, SocketFlags.None, new AsyncCallback(ReceiveData), _clientSocket);
+        }
+        void AddNames(string check)
+        {
+            int index = listBox1.Items.Count;
+            bool found = false;
+
+            if (!check.EndsWith("\r\n"))
+                check = check + "\r\n";
+
+            if (index != 0)
+            {
+                for (int i = 0; i < index; i++)
+                {    
+                    if (listBox1.Items[i].ToString().Contains(check.Substring(2, check.Length - 2)) == false)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    //if (_Name.Replace("@", "").Contains(check.Replace(">", "").Replace("\r\n", "")))
+                    if(!listBox1.Items.Contains(check))
+                    {
+                        listBox1.Items.Add(check);
+                    }
+                }
+
+            }
+            else
+            {
+
+                listBox1.Items.Add(check);
+            }
+        }
+        bool CheckIfDisconnect(string check, Socket socket)
+        {
+            if (check == "*****")
+            {
+                statusLabel.Text = "Disconnected from server.";
+                _clientSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, true);
+                _clientSocket.Disconnect(false);
+                _clientSocket = null;
+                _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                return true;
+            }
+            return false;
+        }
+        private  void LoopConnect()
+        {
+            int attempts = 0;
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect(IPAddress.Loopback, 10010);
+                }
+                catch (SocketException)
+                {
+                    statusLabel.Text = ("Connection attempts: " + attempts.ToString());
+                }
+            }
+            statusLabel.Text = ("Connected!");
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (_clientSocket.Connected)
+            {
+                
+                byte[] buffer = Encoding.ASCII.GetBytes(txt_text.Text);
+                _clientSocket.Send(buffer);
+                mainChat.AppendText("You: " + txt_text.Text + " \r\n");
+                txt_text.Clear();
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            LoopConnect();
+            _clientSocket.BeginReceive(receivedBuf, 0, receivedBuf.Length, SocketFlags.None, new AsyncCallback(ReceiveData), _clientSocket);
+            byte[] buffer = Encoding.ASCII.GetBytes("@@" + txtName.Text);
+            string text = Encoding.ASCII.GetString(buffer);
+            _Name = text;
+            _clientSocket.Send(buffer);
+        }
+
+        private void mainChat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void disconBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (_clientSocket.Connected)
+            {
+
+                byte[] buffer = Encoding.ASCII.GetBytes("disconnect");
+                _clientSocket.Send(buffer);
+                mainChat.AppendText("******************\r\n");
+                txt_text.Clear();
+            }
+        }
+    }
+}
